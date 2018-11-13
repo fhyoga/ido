01424a7c1e94870f7eaff0cf989d83aa6a11e705
Dec-F <dec_fan@icloud.com>
2018-11-13 17:48:52 +0800

    add test

--- code/q-1.js
+++ code/q-1.js
@@ -9,18 +9,11 @@ var twoSum = function(nums, target) {
   nums.forEach((v, i) => {
     let t = target - v;
     if (map.has(t)) {
-      console.log('push');
       res.push(map.get(t), i);
     }
-    console.log(t, map, map.has(t));
     map.set(v, i);
-    console.log(res);
   });
   return res;
 };
 
-let arr = [2, 7, 11, 15];
-target = 9;
-
-var res = twoSum(arr, target);
-console.log(res);
+module.exports = twoSum;
