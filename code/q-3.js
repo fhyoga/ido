3e8529c6c4795ceee208b0bd50db6691686ab02a
Dec-F <dec_fan@icloud.com>
2018-11-14 16:06:39 +0800

    q3

--- code/q-3.js
+++ code/q-3.js
@@ -28,6 +28,23 @@ var lengthOfLongestSubstring = function(s) {
     }
   }
   return max;
+
+  // 另一种
+  // if (!s || s.length == 0) return 0;
+  //   if (s.length == 1) return 1;
+  //   var maxLen = 1;
+  //   var start = 0;
+  //   for (var i = 1; i < s.length; i++) {
+  //   	var c = s[i];
+  //   	var j = s.substring(start,i).indexOf(c)//在start-i前的字符串中找到是s[i],找不到返回-1,找到返回s从左数第一个字符
+  //   	if(j>=0){
+  //   		start +=j + 1;
+  //   	}
+  //   	else {
+  //   		maxLen = Math.max(maxLen,i-start+1)
+  //   	}
+  //   }
+  //   return maxLen;
 };
 
 export default lengthOfLongestSubstring;
