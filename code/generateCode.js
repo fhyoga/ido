ee4bf8737f53da01fc059eb3d9187935ee9ca89c
Dec-F <dec_fan@icloud.com>
2018-12-27 16:42:50 +0800

    active pointer

--- code/generateCode.js
+++ code/generateCode.js
@@ -1,11 +1,21 @@
 const fs = require('fs');
 let argv = process.argv[2];
 
-fs.mkdirSync(`./q${argv}`);
-fs.writeFileSync(`./q${argv}/index.js`, '');
+// 改变已完成题的状态
+fs.readdirSync('./')
+  .filter(file => {
+    return fs.statSync(file).isDirectory() && /active$/.test(file);
+  })
+  .forEach(file => {
+    fs.renameSync(file, file.slice(0, -7));
+  });
+
+// 生成模版
+fs.mkdirSync(`./q${argv}-active`);
+fs.writeFileSync(`./q${argv}-active/index.js`, '');
 
 fs.writeFileSync(
-  `./q${argv}/test.js`,
+  `./q${argv}-active/test.js`,
   `import q${argv} from './';
 describe('', () => {
   test('', () => {
---
+++ code/q71-active/test.js
@@ -0,0 +1,6 @@
+import q71 from './';
+describe('简化路径', () => {
+  test('/home/', () => {
+    expect(q71('/home/')).toBe('/home');
+  });
+});
