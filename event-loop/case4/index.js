472f967775e64f0f20f6f32853eaf2f9a3f6af56
Dec-F <dec_fan@icloud.com>
2020-02-29 13:09:24 +0800

    update

--- event-loop/case4/index.js
+++ event-loop/case4/index.js
@@ -1,14 +1,27 @@
 //渲染几次？
 
 function renderRadio(arguments) {
-  document.getElementById("target").innerHTML = `<input id='radio' type='radio' />`;
+  document.getElementById(
+    "target"
+  ).innerHTML = `<input id='radio' type='radio' />`;
 }
 function checkRadio(arguments) {
   document.getElementById("radio").checked = true;
-  console.log(1);
   setTimeout(() => {
     document.getElementById("radio").checked = false;
   }, 0);
 }
 renderRadio();
 checkRadio();
+
+setTimeout(() => {
+  console.log(2);
+}, 2);
+
+setTimeout(() => {
+  console.log(1);
+}, 1);
+
+setTimeout(() => {
+  console.log(0);
+}, 0);
---
+++ event-loop/readme.md
@@ -0,0 +1,11 @@
+Task queues are sets, not queues, because step one of the event loop processing model grabs the first runnable task from the chosen queue, instead of dequeuing the first task.
+
+### task
+
+- Event
+- _Parsing_
+- Callbacks
+- Using a resource
+- Reacting to DOM manipulation
+
+### microtask
